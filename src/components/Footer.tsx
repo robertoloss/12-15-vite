681d785a2a69a755de7301bd788132cd32ad921e
Roberto Loss <robertoloss@gmail.com>
2024-05-07 17:16:53 -0400

    hearts on contact page and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,6 +43,11 @@ export default function Footer() {
 					</div>
 				</div>
 				<div className="flex flex-col gap-y-2">
+					<div className="flex flex-row w-full justify-end">
+						<svg width="16" height="20" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M28 7.75C28 16.5 15.0262 23.5825 14.4737 23.875C14.3281 23.9533 14.1654 23.9943 14 23.9943C13.8346 23.9943 13.6719 23.9533 13.5262 23.875C12.9737 23.5825 0 16.5 0 7.75C0.00231592 5.69528 0.819575 3.72539 2.27248 2.27248C3.72539 0.819575 5.69528 0.00231592 7.75 0C10.3312 0 12.5912 1.11 14 2.98625C15.4088 1.11 17.6688 0 20.25 0C22.3047 0.00231592 24.2746 0.819575 25.7275 2.27248C27.1804 3.72539 27.9977 5.69528 28 7.75Z" fill="#F96263"/>
+						</svg>
+					</div>
 					<div className="flex flex-row flex-wrap">	
 						<h1 className="mr-1 min-w-[200px]">
 							{website.footer_right_text!.split('©')[0]}
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -83,7 +83,11 @@ export default function Contact() {
   return (<div className='min-h-screen text-foreground'>
 		<AnimationWrapper pageOpen={pageOpen}>
 			<div className="flex flex-col w-full items-center py-20  
-				 px-6 gap-y-4 min-h-[calc(100vh-290px)]">
+				 px-6 gap-y-4 min-h-[calc(100vh-290px)]"
+			>
+				<svg width="28" height="24" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M28 7.75C28 16.5 15.0262 23.5825 14.4737 23.875C14.3281 23.9533 14.1654 23.9943 14 23.9943C13.8346 23.9943 13.6719 23.9533 13.5262 23.875C12.9737 23.5825 0 16.5 0 7.75C0.00231592 5.69528 0.819575 3.72539 2.27248 2.27248C3.72539 0.819575 5.69528 0.00231592 7.75 0C10.3312 0 12.5912 1.11 14 2.98625C15.4088 1.11 17.6688 0 20.25 0C22.3047 0.00231592 24.2746 0.819575 25.7275 2.27248C27.1804 3.72539 27.9977 5.69528 28 7.75Z" fill="#F96263"/>
+				</svg>
 				<h1 className="text-4xl text-center font-semibold text-destructive">
 					Thanks for visiting my website!
 				</h1>
@@ -170,6 +174,7 @@ export default function Contact() {
 					</form>
 				</div>
 				</div>
+
 			</AnimationWrapper>
 		</div>
   );
