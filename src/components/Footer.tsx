af602759dffd2d45e8fe04c5d96194056d667b10
Roberto Loss <robertoloss@gmail.com>
2024-05-07 18:41:49 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,9 +26,8 @@ export default function Footer() {
 
 	return (<>
 		{website && 
-		<div className="flex flex-col w-full h-[210px] items-center justify-center bg-muted text-foreground font-light px-6 gap-y-1">
-			<div className="flex flex-row flex-wrap w-full max-w-[1200px] justify-between items-center gap-y-4">
-				<div className="flex flex-row flex-wrap gap-4 items-center">
+		<div className="flex flex-row flex-wrap w-full h-[210px] items-center justify-between bg-muted text-foreground font-light px-6 gap-y-1">
+				<div className="flex flex-row flex-wrap gap-4 justify-center sm:justify-start items-center min-w-[300px] md:min-w-[400px] flex-1">
 					<a 
 					href={`mailto:${website?.footer_email}`} 
 						target="_blank"
@@ -42,34 +41,28 @@ export default function Footer() {
 					)}
 					</div>
 				</div>
-				<div className="flex flex-col gap-y-2">
-					<div className="flex flex-row w-full justify-end">
+				<div className="flex flex-col items-center sm:items-end gap-y-2 min-w-[300px] md:min-w-[400px] flex-1">
+					<div className="flex flex-row w-full justify-center sm:justify-end">
 						<svg width="16" height="20" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M28 7.75C28 16.5 15.0262 23.5825 14.4737 23.875C14.3281 23.9533 14.1654 23.9943 14 23.9943C13.8346 23.9943 13.6719 23.9533 13.5262 23.875C12.9737 23.5825 0 16.5 0 7.75C0.00231592 5.69528 0.819575 3.72539 2.27248 2.27248C3.72539 0.819575 5.69528 0.00231592 7.75 0C10.3312 0 12.5912 1.11 14 2.98625C15.4088 1.11 17.6688 0 20.25 0C22.3047 0.00231592 24.2746 0.819575 25.7275 2.27248C27.1804 3.72539 27.9977 5.69528 28 7.75Z" fill="#F96263"/>
 						</svg>
 					</div>
-					<div className="flex flex-row flex-wrap">	
-						<h1 className="mr-1 min-w-[200px]">
-							{website.footer_right_text!.split('©')[0]}
+					<div className="flex flex-row justify-end">
+						<h1 className="mr-1 text-center sm:text-right">
+							{website.footer_right_text!.split('©')[0]}©{website.footer_right_text!.split('©')[1]}
 						</h1>
-						<h1 className="mr-1 min-w-[200px] sm:min-w-[0px]">
-							©{website.footer_right_text!.split('©')[1]}
+					</div>
+					<div className="flex flex-row flex-wrap ">	
+						<h1 className="mr-1">
+							Web developer | 	
+						</h1>
+						<h1 className="mr-1 ">
+							<a href="https://robertoloss.netlify.app" target="_blank" className="hover:underline">
+								Roberto Loss						
+							</a>
 						</h1>
 					</div>
 				</div>
-			</div>
-			<div className="flex flex-row flex-wrap w-full max-w-[1200px] justify-start min-[858px]:justify-end items-center gap-y-4">
-				<div className="flex flex-row flex-wrap ">	
-					<h1 className="mr-1">
-						Web developer | 	
-					</h1>
-					<h1 className="mr-1 ">
-						<a href="https://robertoloss.netlify.app" target="_blank" className="hover:underline">
-							Roberto Loss						
-						</a>
-					</h1>
-				</div>
-			</div>
 		</div>}
 	</>)
 }
